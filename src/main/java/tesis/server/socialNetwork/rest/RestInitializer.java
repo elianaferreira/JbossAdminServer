aafbab35a04b3a5d1bd651687556e7f1ba9623c4
eliana <eferreira@roshka.com>
2016-02-17 19:20:33 -0300

    prueba de servicio rest simple para deploy

---
+++ src/main/java/tesis/server/socialNetwork/rest/RestInitializer.java
@@ -0,0 +1,10 @@
+package tesis.server.socialNetwork.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/api/ws")
+public class RestInitializer extends Application{
+
+}
