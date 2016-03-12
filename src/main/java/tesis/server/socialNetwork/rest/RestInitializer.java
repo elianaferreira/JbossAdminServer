7cb2897b32c30f2d85f42fa3c73ded821e6c3511
eliana <eferreira@roshka.com>
2016-03-12 17:59:11 -0300

    Agregada las clases de los servicios Rest al Inicializador para uso de Jersey.

--- src/main/java/tesis/server/socialNetwork/rest/RestInitializer.java
+++ src/main/java/tesis/server/socialNetwork/rest/RestInitializer.java
@@ -1,5 +1,8 @@
 package tesis.server.socialNetwork.rest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -7,4 +10,11 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/api/ws")
 public class RestInitializer extends Application{
 
+	public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+        s.add(VoluntarioWS.class);
+        s.add(PostWS.class);
+        s.add(AdministradorWS.class);
+        return s;
+    }
 }
